13001ffdd469f9e34986ba5069e38fc15b53a07e
kheiywka3-ui <kheiywka3@gmail.com>
2026-07-22 10:43:44 +0700

    website

---
+++ 6904363/coundown/script.js
@@ -0,0 +1,8 @@
+function changeText() {
+    document.getElementById("title").innerHTML = "สวัสดี!";
+    document.getElementById("text").innerHTML = "JavaScript ทำงานเรียบร้อย 🎉";
+}
+
+function toggleDarkMode() {
+    document.body.classList.toggle("dark");
+}
