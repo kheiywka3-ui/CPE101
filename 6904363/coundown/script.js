8fbb90638c183b514fbab6e34e3136d68d688d3a
kheiywka3-ui <kheiywka3@gmail.com>
2026-07-21 20:44:02 -0700

    Delete 6904363/coundown directory

--- 6904363/coundown/script.js
+++
@@ -1,8 +0,0 @@
-function changeText() {
-    document.getElementById("title").innerHTML = "สวัสดี!";
-    document.getElementById("text").innerHTML = "JavaScript ทำงานเรียบร้อย 🎉";
-}
-
-function toggleDarkMode() {
-    document.body.classList.toggle("dark");
-}
